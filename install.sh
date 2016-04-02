2d155e4a167d4cedc3377ebac30f42e3e30142a8
Bran Sorem <bransorem@gmail.com>
2016-04-01 21:29:52 -0700

    mac vs linux

--- install.sh
+++ install.sh
@@ -20,34 +20,38 @@ cd prefs
 ln -s $HOME/code/prefs/zsh/zpreztorc $HOME/.zpreztorc
 ln -s $HOME/code/prefs/zsh/zprofile $HOME/.zprofile
 
-## Homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+## MAC
+if [[ "$(uname)" = "Darwin" ]]; then
+  ## Homebrew
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-## Brew Cask
-brew install caskroom/cask/brew-cask
+  ## Brew Cask
+  brew install caskroom/cask/brew-cask
+  ## Taps
+  brew tap homebrew/services
+
+  ## Casks
+  brew cask install atom cyberduck imageoptim github-desktop firefox macdown namechanger vagrant virtualbox vlc skitch dropbox bartender calibre duet evernote macpaw-gemini jump-desktop iterm2 little-snitch proxpn  techstoreclub-simple-comic sketch transmission unrarx the-unarchiver google-chrome alfred qlmarkdown 1password
+
+  ## Brews
+  brew install boost cmake curl docker elasticsearch git go httpie imagemagick libssh2 libyaml mcrypt mongodb openssl pkg-config rbenv redis rethinkdb sqlite wget
+
+  ## OpenCV
+  brew tap homebrew/science
+  brew install opencv
+
+  # brew cleanup
+  brew cleanup
+  brew cask cleanup
+
+  # iterm2 prefs
+  cp $HOME/code/prefs/iterm2.xml $HOME/Documents/iTerm/com.googlecode.iterm2.plist
+else
+  # linux
+  apt-get update
+fi
 
 ## NVM
 curl https://raw.github.com/creationix/nvm/master/install.sh | sh
-
-## Taps
-brew tap homebrew/services
-
-## Casks
-brew cask install atom cyberduck imageoptim github-desktop firefox macdown namechanger vagrant virtualbox vlc skitch dropbox bartender calibre duet evernote macpaw-gemini jump-desktop iterm2 little-snitch proxpn  techstoreclub-simple-comic sketch transmission unrarx the-unarchiver google-chrome alfred qlmarkdown 1password
-
-## Brews
-brew install boost cmake curl docker elasticsearch git go httpie imagemagick libssh2 libyaml mcrypt mongodb openssl pkg-config rbenv redis rethinkdb sqlite wget
-
-
-## OpenCV
-brew tap homebrew/science
-brew install opencv
-
-# brew cleanup
-brew cleanup
-brew cask cleanup
-
-# iterm2 prefs
-cp $HOME/code/prefs/iterm2.xml $HOME/Documents/iTerm/com.googlecode.iterm2.plist
 
 source $HOME/.zpreztorc
